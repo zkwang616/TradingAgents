d0aefcaceb91e13bb7f99a81be27bf23334a7b0f
zkwang616 <wzk1420347825@163.com>
2026-08-15 10:57:52 +0800

    Revert "Merge branch 'backtest-harness'"
    This reverts commit a9b54c5b1e60e273af7c7d0f8827c171bef5ac23, reversing
    changes made to 2c7ea51b875543c004c925a5bbdeda711b894e1f.

--- tradingagents/evaluation/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Evaluation tooling: reproducible backtesting for the TradingAgents graph."""
-
-from .backtest import generate_dates, run_backtest, summarize
-
-__all__ = ["generate_dates", "run_backtest", "summarize"]
